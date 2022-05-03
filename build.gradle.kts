5ded20b7e7604f03238ffb7211b65eed78c8ce30
vonbrank <vonbrank@outlook.com>
2022-05-03 08:51:56 +0800

    add project libraries

--- build.gradle.kts
+++ build.gradle.kts
@@ -55,9 +55,7 @@ repositories {
 }
 
 dependencies {
-    implementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine:5.8.2")
+    implementation(fileTree("lib") { include("*.jar") })
 }
 
 tasks.getByName<Test>("test") {
