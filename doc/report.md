6db42062e64f77fb7377f7308ea2660537e0dbb6
vonbrank <vonbrank@outlook.com>
2022-05-14 19:05:44 +0800

    add report; update GraphInstanceTest

---
+++ doc/report.md
@@ -0,0 +1,111 @@
+![](https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSiv49DJqSGUcZjoRnB8eIJo1b9wcqqcVpllg&usqp=CAU)
+
+<h1 style="text-align:center">
+    <p>2022年春季学期计算学部《软件构造》课程</p>
+    <p>Lab 2 实验报告</p>
+<h1>
+
+
+
+|   姓名   |        VonBrank        |
+| :------: | :------------------: |
+|   学号   |      120L02****      |
+|   班号   |       20030**        |
+| 电子邮件 | vonbrank@outlook.com |
+| 手机号码 |           |
+
+
+
+## 1 实验目标概述
+
+
+
+## 2 实验环境配置
+
+
+
+## 3 实验过程
+
+
+
+### 3.1 Poetic Walks
+
+#### 3.1.1 Get the code and prepare Git repository
+
+#### 3.1.2 Problem 1: Test Graph \<String\>
+
+#### 3.1.3 Problem 2: Implement Graph \<String\>
+
+##### 3.1.3.1 Implement ConcreteEdgesGraph 
+
+##### 3.1.3.2 Implement ConcreteVerticesGraph 
+
+#### 3.1.4 Problem 3: Implement generic Graph<L> 
+
+##### 3.1.4.1 Make the implementations generic 
+
+##### 3.1.4.2 Implement Graph.empty() 
+
+#### 3.1.5 Problem 4: Poetic walks 
+
+##### 3.1.5.1 Test GraphPoet 
+
+##### 3.1.5.2 Implement GraphPoet 
+
+##### 3.1.5.3 Graph poetry slam 
+
+#### 3.1.6 使用Eclemma检查测试的代码覆盖度
+
+### 3.2 Re-implement the Social Network in Lab1
+
+#### 3.2.1 FriendshipGraph类
+
+#### 3.2.2 Person 类
+
+#### 3.2.3 客户端 main()
+
+#### 3.2.4 测试用例
+
+#### 3.2.5 提交至 Git 仓库
+
+
+
+## 4 实验进度
+
+| 日期 | 时间段 | 任务 | 实际完成情况 |
+| :--: | :----: | :--: | :----------: |
+|      |        |      |              |
+|      |        |      |              |
+|      |        |      |              |
+
+
+
+## 5 实验过程中遇到的困难与解决途径
+
+| 遇到难点 | 解决途径 |
+| :------: | :------: |
+|          |          |
+|          |          |
+|          |          |
+
+
+
+## 6 实验过程中收获的经验、教训、感想
+
+6.1 实验过程中收获的经验和教训（必答） 
+
+6.2 针对以下方面的感受（必答）
+
++ 面向ADT的编程和直接面向应用场景编程，你体会到二者有何差异？ 
+
++ 使用泛型和不使用泛型的编程，对你来说有何差异？
+
++ 在给出ADT的规约后就开始编写测试用例，优势是什么？你是否能够适应这种测试方式？ 
+
++ P1设计的ADT在多个应用场景下使用，这种复用带来什么好处？
+
++ 为ADT撰写specification, invariants, RI, AF，时刻注意ADT是否有rep exposure，这些工作的意义是什么？你是否愿意在以后编程中坚持这么做？
+
++ 关于本实验的工作量、难度、deadline。
+
++ 《软件构造》课程进展到目前，你对该课程有何收获和建议？
--- test/P1/graph/GraphInstanceTest.java
+++ test/P1/graph/GraphInstanceTest.java
@@ -6,41 +6,74 @@ package graph;
 import static org.junit.Assert.*;
 
 import java.util.Collections;
+import java.util.Map;
 
 import org.junit.Test;
 
 /**
  * Tests for instance methods of Graph.
- * 
+ *
  * <p>PS2 instructions: you MUST NOT add constructors, fields, or non-@Test
  * methods to this class, or change the spec of {@link #emptyInstance()}.
  * Your tests MUST only obtain Graph instances by calling emptyInstance().
  * Your tests MUST NOT refer to specific concrete implementations.
  */
 public abstract class GraphInstanceTest {
-    
+
     // Testing strategy
     //   TODO
-    
+
     /**
      * Overridden by implementation-specific test classes.
-     * 
+     *
      * @return a new empty graph of the particular implementation being tested
      */
     public abstract Graph<String> emptyInstance();
-    
-    @Test(expected=AssertionError.class)
+
+    @Test(expected = AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     @Test
     public void testInitialVerticesEmpty() {
         // TODO you may use, change, or remove this test
         assertEquals("expected new graph to have no vertices",
                 Collections.emptySet(), emptyInstance().vertices());
     }
-    
+
     // TODO other tests for instance methods of Graph
-    
+
+    @Test
+    public void testInstanceMethodsOfGraph() {
+        Graph<String> graph = emptyInstance();
+        assertTrue(graph.add("1"));
+        assertTrue(graph.add("3"));
+        assertFalse(graph.add("1"));
+        assertTrue(graph.add("2"));
+        assertTrue(graph.add("5"));
+        assertFalse(graph.add("3"));
+        assertTrue(graph.add("0"));
+        assertTrue(graph.add("4"));
+
+        assertEquals(0, graph.set("0", "2", 1));
+        assertEquals(0, graph.set("0", "4", 4));
+        assertEquals(0, graph.set("0", "5", 2));
+        assertEquals(1, graph.set("0", "2", 4));
+        assertEquals(0, graph.set("1", "4", 8));
+        assertEquals(0, graph.set("1", "5", 5));
+        assertEquals(0, graph.set("2", "3", 7));
+        assertEquals(0, graph.set("2", "4", 3));
+        assertEquals(5, graph.set("1", "5", 1));
+        assertEquals(0, graph.set("4", "5", 5));
+        assertEquals(8, graph.set("1", "4", 8));
+
+        assertTrue(graph.remove("1"));
+        assertFalse(graph.remove("6"));
+
+        assertEquals(Map.of("5", 2, "4", 4, "2", 4), graph.sources("0"));
+        assertEquals(Map.of("0", 4, "2", 3), graph.targets("4"));
+
+    }
+
 }
