546f5cfaac4f08492026be474ae2bce92e03d068
vonbrank <vonbrank@outlook.com>
2022-05-19 11:40:52 +0800

    update GraphPoetTest and report

--- test/P1/poet/GraphPoetTest.java
+++ test/P1/poet/GraphPoetTest.java
@@ -11,15 +11,18 @@ import org.junit.Test;
  * Tests for GraphPoet.
  */
 public class GraphPoetTest {
-    
+
     // Testing strategy
     //   TODO
-    
+
     @Test(expected=AssertionError.class)
     public void testAssertionsEnabled() {
         assert false; // make sure assertions are enabled with VM argument: -ea
     }
-    
+
     // TODO tests
-    
+    @Test
+    public void testGraphPoet() {
+
+    }
 }
