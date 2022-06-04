d11ef296dd2ba9591d1ecda2aebb6f27c121a0f9
vonbrank <vonbrank@outlook.com>
2022-06-04 16:51:12 +0800

    feat(Task-03): complete implementations of Vote

--- src/vote/Vote.java
+++ src/vote/Vote.java
@@ -2,70 +2,74 @@ package vote;
 
 import java.util.Calendar;
 import java.util.HashSet;
+import java.util.Objects;
 import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
 
 //immutable
 public class Vote<C> {
 
-	// 缺省为“匿名”投票，即不需要管理投票人的信息
+    // 缺省为“匿名”投票，即不需要管理投票人的信息
 
-	// 一个投票人对所有候选对象的投票项集合
-	private Set<VoteItem<C>> voteItems = new HashSet<>();
-	// 投票时间
-	private Calendar date = Calendar.getInstance();
+    // 一个投票人对所有候选对象的投票项集合
+    private Set<VoteItem<C>> voteItems = new HashSet<>();
+    // 投票时间
+    private Calendar date = Calendar.getInstance();
 
-	// Rep Invariants
-	// TODO
-	// Abstract Function
-	// TODO
-	// Safety from Rep Exposure
-	// TODO
+    // Rep Invariants
+    //   voteItems 和 date 都不为 null 且 Vote<C> 本身是 immutable 的
+    // Abstract Function
+    //   AF(voteItems, date) = 一次投票，voteItems 包含了改投票的所有内容，date 是这次投票的创建时间
+    // Safety from Rep Exposure
+    //   Vote<C> 是 immutable 的，voteItems 和 date 只在构造函数内被赋值，没有其他 mutator
+    //   返回 voteItems 的方法将执行防御式拷贝
 
-	private boolean checkRep() {
-		// TODO
-		return false;
-	}
+    private boolean checkRep() {
+        return voteItems != null && date != null;
+    }
 
-	/**
-	 * 创建一个选票对象
-	 * 
-	 * 可以自行设计该方法所采用的参数
-	 * 
-	 */
-	public Vote(/* TODO */) {
-		// TODO
-	}
+    /**
+     * 创建一个选票对象
+     * <p>
+     * 可以自行设计该方法所采用的参数
+     */
+    public Vote(Set<VoteItem<C>> voteItems) {
+        this.voteItems.addAll(voteItems);
+    }
 
-	/**
-	 * 查询该选票中包含的所有投票项
-	 * 
-	 * @return 所有投票项
-	 */
-	public Set<VoteItem<C>> getVoteItems() {
-		// TODO
-		return null;
-	}
+    /**
+     * 查询该选票中包含的所有投票项
+     *
+     * @return 所有投票项
+     */
+    public Set<VoteItem<C>> getVoteItems() {
+        return new HashSet<>(voteItems);
+    }
 
-	/**
-	 * 一个特定候选人是否包含本选票中
-	 * 
-	 * @param candidate 待查询的候选人
-	 * @return 若包含该候选人的投票项，则返回true，否则false
-	 */
-	public boolean candidateIncluded(C candidate) {
-		// TODO
-		return false;
-	}
+    /**
+     * 一个特定候选人是否包含本选票中
+     *
+     * @param candidate 待查询的候选人
+     * @return 若包含该候选人的投票项，则返回true，否则false
+     */
+    public boolean candidateIncluded(C candidate) {
+        AtomicBoolean res = new AtomicBoolean(false);
+        voteItems.forEach(voteItem -> {
+            if (voteItem.getCandidate().equals(candidate)) res.set(true);
+        });
+        return res.get();
+    }
 
-	@Override
-	public int hashCode() {
-		// TODO
-		return 0;
-	}
+    @Override
+    public int hashCode() {
+        return Objects.hash(voteItems);
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		// TODO
-		return true;
-	}
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote<?> vote = (Vote<?>) o;
+        return voteItems.equals(vote.voteItems);
+    }
 }
