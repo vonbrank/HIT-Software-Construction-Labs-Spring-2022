5d12ca046095d3cd302aea484f2c90a71da697ea
vonbrank <vonbrank@outlook.com>
2022-06-04 16:50:16 +0800

    test(Task-03): add unit test for Vote

--- test/vote/VoteTest.java
+++ test/vote/VoteTest.java
@@ -2,16 +2,43 @@ package vote;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import auxiliary.Dish;
 import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
 
 class VoteTest {
 
 	// test strategy
 	// TODO
-	
+
 	@Test
 	void test() {
-		fail("Not yet implemented");
+
+		Set<VoteItem<Dish>> dishSet = new HashSet<>(Set.of(
+				new VoteItem<>(new Dish("A", 10), "喜欢"),
+				new VoteItem<>(new Dish("B", 15), "不喜欢")
+		));
+
+		Vote<Dish> vote = new Vote<Dish>(dishSet);
+
+		assertEquals(dishSet, vote.getVoteItems());
+
+		assertTrue(vote.candidateIncluded(new Dish("A", 10)));
+
+		Vote<Dish> otherVote = new Vote<Dish>(dishSet);
+
+		assertEquals(vote, otherVote);
+		assertEquals(otherVote.hashCode(), vote.hashCode());
+
+		dishSet.remove(new VoteItem<Dish>(new Dish("A", 10), "喜欢"));
+		dishSet.add(new VoteItem<Dish>(new Dish("A", 11), "喜欢"));
+		otherVote = new Vote<Dish>(dishSet);
+
+		assertNotEquals(otherVote, vote);
+		assertNotEquals(otherVote.hashCode(), vote.hashCode());
+
 	}
 
 }
