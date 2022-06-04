4e1af1c18c276ec12e2eed128bd8b89c4d8abbf8
vonbrank <vonbrank@outlook.com>
2022-06-04 11:50:50 +0800

    test: add unit test for VoteItem<C>

--- test/vote/VoteItemTest.java
+++ test/vote/VoteItemTest.java
@@ -2,16 +2,29 @@ package vote;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import auxiliary.Dish;
 import org.junit.jupiter.api.Test;
 
 class VoteItemTest {
 
-	// test strategy
-	// TODO
-	
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+    // test strategy
+
+    @Test
+    void test() {
+
+        VoteItem<Dish> voteItem = new VoteItem<>(new Dish("A", 10), "喜欢");
+
+        assertEquals("喜欢", voteItem.getVoteValue());
+        assertNotEquals("讨厌", voteItem.getVoteValue());
+
+        assertEquals(new VoteItem<>(new Dish("A", 10), "喜欢"), voteItem);
+        assertNotEquals(new VoteItem<>(new Dish("A", 11), "喜欢"), voteItem);
+        assertNotEquals(new VoteItem<>(new Dish("A", 10), "不喜欢"), voteItem);
+
+        assertEquals(new VoteItem<>(new Dish("A", 10), "喜欢").hashCode(), voteItem.hashCode());
+        assertNotEquals(new VoteItem<>(new Dish("A", 11), "喜欢").hashCode(), voteItem.hashCode());
+        assertNotEquals(new VoteItem<>(new Dish("A", 10), "不喜欢").hashCode(), voteItem.hashCode());
+
+    }
 
 }
