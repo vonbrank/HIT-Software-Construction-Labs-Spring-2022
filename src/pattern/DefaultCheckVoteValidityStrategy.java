02bb83f494b271452e7e901d7489c3216371650f
vonbrank <vonbrank@outlook.com>
2022-06-22 18:33:15 +0800

    feat: 完成计票有效性检查策略

---
+++ src/pattern/DefaultCheckVoteValidityStrategy.java
@@ -0,0 +1,28 @@
+package pattern;
+
+import vote.Vote;
+import vote.VoteType;
+
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicBoolean;
+
+public class DefaultCheckVoteValidityStrategy implements CheckVoteValidityStrategy {
+    @Override
+    public <C> boolean checkVoteValidity(List<C> targetCandidates, Vote<C> newVote, Set<Vote<C>> polledVotes, VoteType voteType) {
+        AtomicBoolean voteValid = new AtomicBoolean(true);
+        targetCandidates.forEach(candidate -> {
+            if(!newVote.candidateIncluded(candidate)) voteValid.set(false);
+        });
+        Set<C> polledCandidates = new HashSet<>();
+        newVote.getVoteItems().forEach(voteItem -> {
+            if(!targetCandidates.contains(voteItem.getCandidate())) voteValid.set(false);
+            if(!voteType.checkLegality(voteItem.getVoteValue())) voteValid.set(false);
+            if(polledCandidates.contains(voteItem.getCandidate())) voteValid.set(false);
+            polledCandidates.add(voteItem.getCandidate());
+        });
+        return voteValid.get();
+    }
+}
+
---
+++ src/pattern/ElectionCheckVoteValidityStrategy.java
@@ -0,0 +1,32 @@
+package pattern;
+
+import vote.Vote;
+import vote.VoteType;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.atomic.AtomicInteger;
+
+public class ElectionCheckVoteValidityStrategy extends DefaultCheckVoteValidityStrategy {
+    // 支持票数量上限
+    private final int quantity;
+
+    /**
+     * 构造选举投票专用的投票有效性检查策略
+     * @param quantity 支持票数量上限
+     */
+    ElectionCheckVoteValidityStrategy(int quantity) {
+        this.quantity = quantity;
+    }
+
+    @Override
+    public <C> boolean checkVoteValidity(List<C> targetCandidates, Vote<C> newVote, Set<Vote<C>> polledVotes, VoteType voteType) {
+        boolean res = super.checkVoteValidity(targetCandidates, newVote, polledVotes, voteType);
+        AtomicInteger voteQuantity = new AtomicInteger();
+        newVote.getVoteItems().forEach(voteItem -> {
+            if (voteItem.getVoteValue().equals("支持")) voteQuantity.getAndIncrement();
+        });
+        if (voteQuantity.get() > quantity) res = false;
+        return res;
+    }
+}
