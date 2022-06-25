9914ecb48055d712995bcaca0614b44c4965bacf
vonbrank <vonbrank@outlook.com>
2022-06-25 19:34:01 +0800

    chore: 将依赖项路径设置为 ./lib ，并添加 Github Action 持续集成

--- build.gradle.kts
+++ build.gradle.kts
@@ -8,9 +8,21 @@ version = "1.0-SNAPSHOT"
 sourceSets {
     main {
         java.srcDir("./src")
-        task<Jar>("main") {
-            archiveBaseName.set("main")
-            manifest.attributes("Main-Class" to "ElectionApp")
+        task<Jar>("ElectionApp") {
+            archiveBaseName.set("ElectionApp")
+            manifest.attributes("Main-Class" to "app.ElectionApp")
+            dependsOn(runtimeClasspath)
+            from(runtimeClasspath)
+        }
+        task<Jar>("DinnerOrderApp") {
+            archiveBaseName.set("DinnerOrderApp")
+            manifest.attributes("Main-Class" to "app.DinnerOrderApp")
+            dependsOn(runtimeClasspath)
+            from(runtimeClasspath)
+        }
+        task<Jar>("BusinessVotingApp") {
+            archiveBaseName.set("BusinessVotingApp")
+            manifest.attributes("Main-Class" to "app.BusinessVotingApp")
             dependsOn(runtimeClasspath)
             from(runtimeClasspath)
         }
@@ -25,7 +37,7 @@ sourceSets {
 
 }
 
-tasks["build"].dependsOn("main")
+tasks["build"].dependsOn("ElectionApp", "DinnerOrderApp", "BusinessVotingApp")
 
 java {
     sourceCompatibility = org.gradle.api.JavaVersion.VERSION_1_10
@@ -37,9 +49,7 @@ repositories {
 }
 
 dependencies {
-    implementation("junit:junit:4.13.2")
-    testImplementation("org.junit.jupiter:junit-jupiter-api:5.8.2")
-    testRuntimeOnly("org.junit.jupiter:junit-jupiter-engine")
+    implementation(fileTree("lib") { include("*.jar") })
 }
 
 tasks.getByName<Test>("test") {
