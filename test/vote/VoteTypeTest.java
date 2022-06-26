f9d3aca567823020d9392ff637a1734e8d7b1994
vonbrank <vonbrank@outlook.com>
2022-06-26 10:41:01 +0800

    refactor: VoteType 的单元测试代码移动到正确位置

--- test/vote/VoteTypeTest.java
+++ test/vote/VoteTypeTest.java
@@ -2,6 +2,7 @@ package vote;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import exception.StringFormatException;
 import org.junit.jupiter.api.Test;
 
 import java.util.HashMap;
@@ -42,6 +43,46 @@ class VoteTypeTest {
 		otherVoteType = new VoteType(otherOptions);
 		assertEquals(voteType, otherVoteType);
 		assertEquals(voteType.hashCode(), otherVoteType.hashCode());
+
+	}
+
+	/*
+	 *  Testing strategy
+	 *
+	 *  1. 英语，5 个字符以内   "Yes"(1)|"No"(-1)|"SoSo"(0)
+	 *  2. 英语，超过 5 个字符  "Support"(1)|"Oppose"(-1)|"Waive"(0)
+	 *  3. 汉语，5 个字符以内   "支持"(1)|"不支持"(-1)|"无所谓"(0)
+	 *  4. 汉语，超过 5 个字符  "超级无敌支持"(1)|"不支持"(-1)|"无所谓"(0)
+	 *  5. 英语，无分数        "Yes"|"No"|"SoSo"
+	 *  6. 汉语，无分数        "支持"|"不支持"|"无所谓"
+	 */
+
+	@Test
+	void testRegexConstructor() {
+		try {
+			VoteType voteType1 = new VoteType("\"Yes\"(1)|\"No\"(-1)|\"SoSo\"(0)");
+			System.out.println(voteType1);
+
+			assertThrows(StringFormatException.class, () -> {
+				VoteType voteType2 = new VoteType("\"Support\"(1)|\"Oppose\"(-1)|\"Waive\"(0)");
+			});
+
+			VoteType voteType3 = new VoteType("\"支持\"(1)|\"不支持\"(-1)|\"无所谓\"(0)");
+			System.out.println(voteType3);
+
+			assertThrows(StringFormatException.class, () -> {
+				VoteType voteType4 = new VoteType("\"超级无敌支持\"(1)|\"不支持\"(-1)|\"无所谓\"(0)");
+			});
+
+			VoteType voteType5 = new VoteType("\"Yes\"|\"No\"|\"SoSo\"");
+			System.out.println(voteType5);
+
+			VoteType voteType6 = new VoteType("\"支持\"|\"不支持\"|\"无所谓\"");
+			System.out.println(voteType6);
+
+		} catch (StringFormatException e) {
+			e.printStackTrace();
+		}
 
 	}
 
